cb058e1d831e7c5e807254aac1b61d0747404f59
Moraniel Junior <moraniel@outlook.com>
2025-02-04 08:28:15 -0300

    modificacoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardVideoComponent } from "./components/card-video/card-video.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardVideoComponent,MenuBarComponent, ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
