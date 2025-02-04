f4fd40fd964fff34075a768d1cb4a078e1286bda
Moraniel Junior <moraniel@outlook.com>
2025-02-04 09:27:24 -0300

    Parte vizual dos cards finalizada

--- src/app/components/card-video/card-video.component.ts
+++ src/app/components/card-video/card-video.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input,  } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-card-video',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './card-video.component.html',
   styleUrl: './card-video.component.css'
 })
 export class CardVideoComponent {
 
+  @Input() titulo:string=''
+
+    constructor(){
+      this.titulo
+    }
 }
